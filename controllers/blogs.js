7dfba1a338cf56d4178ea2a3e197a31909fb8d98
= <salaslilia00@gmail.com>
2022-03-14 22:08:24 -0400

    created index and show route

---
+++ controllers/blogs.js
@@ -0,0 +1,36 @@
+const Blog = require('../models/blog');
+const Comment = require('../models/comment');
+
+const router = require('express').Router();
+
+//create
+
+//read (index & show)
+
+//index
+router.get('/', async(req, res) => {
+    try{
+        const foundBlogs = await Blog.find({})
+        res.status(200).json(foundBlogs)
+    }catch(error){
+        console.error(err);
+        res.status(400).json({ message: error.message });
+    }
+}
+
+
+// show
+
+router.get('/:id', async(req, res) => {
+    try{
+        const foundBlog = await Blog.findById(req.params.id)
+        res.status(200).json(foundBlog)
+    }catch(error){
+        console.error(err);
+        res.status(400).json({ message: error.message });
+    }
+}
+
+//update
+
+//delete
--- models/comment.js
+++ models/comment.js
@@ -3,11 +3,12 @@ const { Schema, model } = require('mongoose');
 const commentSchema = new Schema(
 	{
 		name: { type: String, required: true },
-		message: { type: String, required: true }
+		message: { type: String, required: true },
+		comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
 	},
 	{
 		timestamps: true
 	}
 );
 
-module.exports = mode('Comment', commentSchema);
+module.exports = model('Comment', commentSchema);
