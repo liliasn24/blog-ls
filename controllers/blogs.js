30d797bf95a2dae1789f4a638017142d3236fd55
= <salaslilia00@gmail.com>
2022-03-17 22:28:02 -0400

    fixed the server file, controller

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -5,88 +5,94 @@ const router = require('express').Router();
 
 //create
 
-router.post('/', async(req, res) => {
-  try{
-    const createdBlog = await Blog.create(req.body)
-    res.status(200).json(createdBlog)
-  }catch(error){
-    console.error(error)
-    res.status(400).json({ message: error.message})
-  }
-})
-
+router.post('/', async (req, res) => {
+	try {
+		const createdBlog = await Blog.create(req.body);
+		res.status(200).json(createdBlog);
+	} catch (error) {
+		console.error(error);
+		res.status(400).json({ message: error.message });
+	}
+});
 
 //read (index & show)
 
 //index
-router.get('/', async(req, res) => {
-    try{
-        const foundBlogs = await Blog.find({})
-        res.status(200).json(foundBlogs)
-    }catch(error){
-        console.error(error);
-        res.status(400).json({ message: error.message });
-    }
-}
-
+router.get('/', async (req, res) => {
+	try {
+		const foundBlogs = await Blog.find({});
+		res.status(200).json(foundBlogs);
+	} catch (error) {
+		console.error(error);
+		res.status(400).json({ message: error.message });
+	}
+});
 
 // show
 
-router.get('/:id', async(req, res) => {
-    try{
-        const foundBlog = await Blog.findById(req.params.id)
-        res.status(200).json(foundBlog)
-    }catch(error){
-        console.error(error);
-        res.status(400).json({ message: error.message });
-    }
-}
+router.get('/:id', async (req, res) => {
+	try {
+		const foundBlog = await Blog.findById(req.params.id);
+		res.status(200).json(foundBlog);
+	} catch (error) {
+		console.error(error);
+		res.status(400).json({ message: error.message });
+	}
+});
 
 //update
 //update the blog
 router.put('/:id', async (req, res) => {
-    try {
-      const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, { new: true })
-      res.status(200).json(updatedBlog)
-    }catch(error){
-      console.error(error);
-      res.status(400).json({ message: error.message })
-    }
-  })
+	try {
+		const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+			new: true
+		});
+		res.status(200).json(updatedBlog);
+	} catch (error) {
+		console.error(error);
+		res.status(400).json({ message: error.message });
+	}
+});
 
 //add comment
 
 router.put('/:id/addComment', (req, res) => {
-    //store the query
-    const createCommentQuery = Comment.create(req.body)
-    // actually run query
-    createCommentQuery.exec((err, createdComment) => {
-      if (err){
-        console.error(err);
-        res.status(400).json({ message: err.message});
-      } else {
-        const updateBlogQuery = Blog.findByIdAndUpdate(req.params.id, { $addToSet: { comments: createdComment._id }}, { new: true })
-        // actually run it
-        updateBlogQuery.exec((err, updatedBlog) => {
-              if(err){
-                console.error(err);
-                res.status(400).json({ message: err.message })
-              } else {
-                res.status(200).json(createdComment)
-              }
-        })
-      }
-    })
-  })
+	//store the query
+	const createCommentQuery = Comment.create(req.body);
+	// actually run query
+	createCommentQuery.exec((err, createdComment) => {
+		if (err) {
+			console.error(err);
+			res.status(400).json({ message: err.message });
+		} else {
+			const updateBlogQuery = Blog.findByIdAndUpdate(
+				req.params.id,
+				{ $addToSet: { comments: createdComment._id } },
+				{ new: true }
+			);
+			// actually run it
+			updateBlogQuery.exec((err, updatedBlog) => {
+				if (err) {
+					console.error(err);
+					res.status(400).json({ message: err.message });
+				} else {
+					res.status(200).json(createdComment);
+				}
+			});
+		}
+	});
+});
 
 //delete
 
 router.delete('/:id', async (req, res) => {
-  try{
-    const deletedBlog = await Blog.findByIdAndDelete(req.params.id);
-    res.status(200).json(deletedBlog)
-  }catch(error){
-    console.error(error);
-    res.status(400).json({ message: error.message})
-  }
-})
+	try {
+		const deletedBlog = await Blog.findByIdAndDelete(req.params.id);
+		res.status(200).json(deletedBlog);
+	} catch (error) {
+		console.error(error);
+		res.status(400).json({ message: error.message });
+	}
+});
+
+module.exports = router;
