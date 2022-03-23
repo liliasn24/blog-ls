499b9abb9ae4d0a2ab6a2f2b4a5a829e9575c321
= <salaslilia00@gmail.com>
2022-03-22 22:35:04 -0400

    updated home page to show the blogs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Home(props) {
-	return <div className="HomePage">This is the {props.page} page</div>;
+	const [blogs, setBlogs] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await fetch('/api/blogs');
+				const data = await response.json();
+				setBlogs(data);
+			} catch (error) {
+				console.error(error);
+			}
+		})();
+	}, []);
+
+	return (
+		<div className="HomePage">
+			<ul>
+				{blogs.map(blog => {
+					return (
+						<li key={blog._id}>
+							<h3>{blog.title}</h3>
+							<p>{blog.body}</p>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
