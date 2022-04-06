64a4963adbf77280936b9deb6dbdcf80232b6ff3
= <salaslilia00@gmail.com>
2022-04-05 22:51:21 -0400

    added the update route

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import { response } from 'express';
 import React, { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -12,13 +13,15 @@ export default function Home(props) {
 			const response = await fetch(`/api/blogs/${props.match.params.id}`), {
 				method: 'PUT',
 				headers: {
-					'Content-Type': 'application/json'
+					Content-Type: 'application/json'
 				},
-				body:JSON.stringify({
+				body: JSON.stringify({
 					title: titleInput.current.value,
 					body: bodyInput.current.value
 				})
 			})
+			const data = await response.json()
+			setBlogs(data)
 		} catch (error) {
 			console.error(error);
 		}
