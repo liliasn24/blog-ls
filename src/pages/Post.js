d2f673a851ca7bc42d52bc5d0690b38c8c1a1385
= <salaslilia00@gmail.com>
2022-04-06 17:28:29 -0400

    form to update the blog

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -64,6 +64,21 @@ export default function Show(props) {
 			) : (
 				<h1>Loading...</h1>
 			)}
+			<form
+				style={{ display: 'flex', flexDirection: 'column' }}
+				onSubmit={handleUpdate}
+			>
+				<label>
+					{' '}
+					Title:{' '}
+					<input type="text" ref={titleInput} defaultValue={blog.title} />
+				</label>
+				<label>
+					{' '}
+					Body: <input type="text" ref={bodyInput} defaultValue={blog.body} />
+				</label>
+				<input type="submit" value="Update MicroBlog" />
+			</form>
 		</div>
 	);
 }
