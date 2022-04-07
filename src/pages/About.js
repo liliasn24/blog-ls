a0a101dea9acd4d09df19bb1c3c7227182309d09
= <salaslilia00@gmail.com>
2022-04-07 17:47:25 -0400

    added basic contact info

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,21 @@
 import React, { useState } from 'react';
 
 export default function About(props) {
-	return <div className="AboutPage">This is the {props.page} page</div>;
+	return (
+		<div class="jumbotron">
+			<div>
+				<h1>About me</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+					minima perspiciatis autem reprehenderit architecto, mollitia corporis
+					esse corrupti nam sed distinctio eligendi earum! Nulla deleniti
+					placeat magnam dolores ratione alias impedit est a magni pariatur
+					debitis vitae et sequi corporis dicta explicabo illo soluta
+					accusantium porro, praesentium laboriosam nam laudantium.
+				</p>
+			</div>
+
+			<img></img>
+		</div>
+	);
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,28 @@
 import React, { useState } from 'react';
 
 export default function Contact(props) {
-	return <div className="ContactPage">This is the {props.page} page</div>;
+	return (
+		<div className="ContactPage">
+			<div class="social">
+				<p>Send me an email</p>
+				<a class="social" href="mailto: salaslilia00@gmail.com">
+					<i class="far fa-envelope-open click"></i>
+				</a>
+			</div>
+
+			<div class="social">
+				<p>Check out my projects on Github</p>
+				<a href="https://github.com/liliasn24" target="_blank">
+					<i class="fab fa-github click"></i>
+				</a>
+			</div>
+
+			<div class="social">
+				<p>Add me on linkedin</p>
+				<a href="https://www.linkedin.com/in/lilia-salas/" target="_blank">
+					<i class="fab fa-linkedin click"></i>
+				</a>
+			</div>
+		</div>
+	);
 }
