adbac063c09310df1eeeb4d3e6849057efcd6b7b
= <salaslilia00@gmail.com>
2022-04-14 21:15:01 -0400

    created the register function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
+const { createSecureServer } = require('http2');
 const jwt = require('jsonwebtoken');
+const User = require('../models/user.js');
 
 // Hashing function
 
@@ -22,9 +24,29 @@ const hash = password => {
 		.join('');
 };
 
-// verification goes here
+module.exports.hash = hash;
 
 // register
+
+const registerService = (req, res) => {
+	const hashedPassword = hash(req.body.password);
+	console.log('hashedPassword:', hashedPassword);
+	req.body.password = bcrypt.hashSync(hashedPassword, bcrypt.genSaltSync(10));
+	console.log(req.body);
+
+	try {
+		const createdUser = await User.create(req.body)
+		const token = jwt.sign({
+			userName:createdUser.username
+		}, SECRET)
+		res.status(200).json({ user: createSecureServer, token })
+	} catch (err) {
+		console.error(err);
+		res.status(400).json({ msg: err.message });
+	}
+};
+
+// verification goes here
 
 // header authentication
 
