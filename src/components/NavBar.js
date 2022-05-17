41868a32cc8b3d37e7031bb659aecc7d044736d8
= <salaslilia00@gmail.com>
2022-05-16 21:30:50 -0400

    navbar looks better

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 const NavBar = props => {
 	return (
 		<nav className="navbar navbar-expand-sm navbar-dark bg-dark p-0">
-			{props.routes.map(({ key, path }) => (
-				<Link key={key} to={path}>
-					{key}
-				</Link>
-			))}
+			<div className="container">
+				{props.routes.map(({ key, path }) => (
+					<Link key={key} to={path}>
+						{key}
+					</Link>
+				))}
+			</div>
 		</nav>
 	);
 };
