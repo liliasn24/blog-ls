a4994f8bd129958769b4796dfcfecbaa2b5874c5
= <salaslilia00@gmail.com>
2022-05-24 22:27:09 -0400

    nav bar back to what it was

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,46 +3,12 @@ import { Link } from 'react-router-dom';
 
 const NavBar = props => {
 	return (
-		<nav
-			class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top"
-			id="main-nav"
-		>
-			<div class="container">
-				<a href="index.html" class="navbar-brand">
-					Lilia Salas
-				</a>
-				<button
-					class="navbar-toggler"
-					data-toggle="collapse"
-					data-target="#navbarCollapse"
-				>
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarCollapse">
-					<ul class="navbar-nav ml-auto">
-						<li class="nav-item">
-							<a href="#home" class="nav-link">
-								Home
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="#explore-head-section" class="nav-link">
-								About
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="#create-head-section" class="nav-link">
-								Contact
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="#share-head-section" class="nav-link">
-								App
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
+		<nav className="NavBar">
+			{props.routes.map(({ key, path }) => (
+				<Link key={key} to={path}>
+					{key}
+				</Link>
+			))}
 		</nav>
 	);
 };
